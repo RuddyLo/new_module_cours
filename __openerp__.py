216a64b579ad23265dc6df2ce5caaeebc403e08c
RuddyLo <ratianarivoruddy@gmail.com>
2020-03-20 09:21:29 +0300

    Creation des fichier init et openerp

---
+++ __openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+# Baamtu, 2017
+# GNU Affero General Public License <http://www.gnu.org/licenses/>
+{
+    "name" : "Module Cours",
+    "version" : "1.1",
+    'license': 'AGPL-3',
+    "author" : "Me",
+    'category': 'Human Resources',
+    'website': 'http://www.cous.com/',
+    #'images': ['static/description/banner.jpg'],
+    'summary': 'Cours , session',
+    'description': """
+Human Resources Management
+==========================
+
+This application enables you to manage important aspects of your company's staff and other details such as their skills, contacts, working time...
+
+
+You can manage:
+---------------
+* Employees and hierarchies : You can define your employee with User and display hierarchies
+* HR Departments
+* HR Jobs
+    """,
+    'depends' : ['base'],
+    'data': [
+        
+        ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
